2f9f925806aaff8b5a7fefe0a83e5bb5b50f1064
zhangdebo11 <zhang_debo@c.tre-inc.com>
2022-11-14 16:43:47 +0800

    local port monitoring

--- README.md
+++ README.md
@@ -1,1 +1,69 @@
-# metrics-sidecar
+# How to Use Metrics-Sidecar
+
+Metrics-Sidecar is a way to monitor local port usage of containers.
+Suppose that we want to monitor the port usage of `manju` container, we should perform the following steps.
+
+## Add a Metrics Container to Target Application Pod
+
+Edit `manju` deployment, add an extra container using image `metrics-sidecar` which is built by this project.
+
+```
+      containers:
+      - image: asia.gcr.io/smartcart-stagingization/metrics-sidecar:develop
+        imagePullPolicy: Always
+        name: metrics
+        ports:
+        - containerPort: 9999
+        resources:
+          limits:
+            cpu: 250m
+            memory: 128Mi
+          requests:
+            cpu: 250m
+            memory: 128Mi
+```
+
+In this new container, there is a mini web server exposing only one API `/metrics`.
+
+## Create a New Service for Metrics-Sidecar
+
+As following, `namespace` and `selector` should be same as target application service `manju`, `targetPort` is the port of metrics service, `annotations` are necessary so that `prometheus` can scrape data from here.
+
+```
+apiVersion: v1
+kind: Service
+metadata:
+  annotations:
+    prometheus.io/path: /metrics
+    prometheus.io/port: "9999"
+    prometheus.io/scrape: "true"
+  labels:
+    app.kubernetes.io/name: manju
+  name: manju-metrics
+  namespace: default
+spec:
+  ports:
+  - name: metrics
+    port: 9999
+    protocol: TCP
+    targetPort: 9999
+  selector:
+    app.kubernetes.io/instance: manju
+    app.kubernetes.io/name: manju
+  type: ClusterIP
+```
+
+# How the Metrics Data Looks Like
+
+```
+# HELP port_used Used Local Port Count
+# TYPE port_used gauge
+port_used{pod_name="manju-7744b786fc-jvftc"} 345
+# HELP port_total Total Local Port Count
+# TYPE port_total gauge
+port_total{pod_name="manju-7744b786fc-jvftc"} 28888
+```
+
+# Where the Data Comes From
+
+`port_used` comes from file `/proc/net/tcp`, `port_total` comes from file `/proc/sys/net/ipv4/ip_local_port_range`.
---
+++ main.go
@@ -0,0 +1,115 @@
+// MIT License
+
+// Copyright (c) The RAI Authors
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"github.com/prometheus/procfs"
+)
+
+var podName string
+var portTotalCount int
+
+func getPodName() {
+	hostname, err := os.Hostname()
+	if err != nil {
+		log.Println("Failed to get pod name.")
+		return
+	}
+	podName = hostname
+}
+
+func getPortTotalCount() {
+	procFS, err := procfs.NewFS("/proc")
+	if err != nil {
+		log.Println("Failed to read /proc.")
+		return
+	}
+
+	portsArr, err := procFS.SysctlStrings("net/ipv4/ip_local_port_range")
+	if err != nil {
+		log.Println("Failed to read local port range.")
+		return
+	}
+	if len(portsArr) < 2 {
+		log.Println("Incorrect format of local port range.")
+		return
+	}
+
+	firstPort, err := strconv.Atoi(portsArr[0])
+	if err != nil {
+		log.Println("Failed to read first local port number.")
+		return
+	}
+	lastPort, err := strconv.Atoi(portsArr[1])
+	if err != nil {
+		log.Println("Failed to read last local port number.")
+		return
+	}
+	portTotalCount = lastPort - firstPort + 1
+}
+
+func getPortUsedCount() int {
+	procFS, err := procfs.NewFS("/proc")
+	if err != nil {
+		log.Println("Failed to read /proc.")
+		return 0
+	}
+
+	netTCP, err := procFS.NetTCP()
+	if err != nil {
+		log.Println("Failed to get proc/net/tcp information.")
+		return 0
+	}
+
+	return len(netTCP)
+}
+
+func metricsHandler(w http.ResponseWriter, r *http.Request) {
+	portUsedCount := getPortUsedCount()
+
+	outputFormat := `# HELP port_used Used Local Port Count
+# TYPE port_used gauge
+port_used{pod_name="%s"} %d
+# HELP port_total Total Local Port Count
+# TYPE port_total gauge
+port_total{pod_name="%s"} %d`
+
+	output := fmt.Sprintf(outputFormat, podName, portUsedCount, podName, portTotalCount)
+
+	w.Write([]byte(output))
+}
+
+func main() {
+	log.Println("Metrics server start...")
+	http.HandleFunc("/metrics", metricsHandler)
+	http.ListenAndServe("0.0.0.0:9999", nil)
+}
+
+func init() {
+	getPodName()
+	getPortTotalCount()
+}
